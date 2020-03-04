b84376525a245e270eb996c40a9031fa183d6995
Alejandro Alcázar <aalcazar@brooktec.com>
2020-03-04 12:54:17 +0100

    Refactor tests and add controller

--- server/graphql-api/tests/graphql.test.js
+++ server/graphql-api/tests/graphql.test.js
@@ -1,29 +1,41 @@
 const request = require('supertest');
 const app = require('../..');
+const controller = require('../controller');
 
 describe('GraphQL Server', () => {
-  it('should expose graphql playground', done => {
-    request(app)
-      .get('/graphql')
-      .set('Accept', 'text/html')
-      .expect(200, done);
+  describe('GraphQL API Integration', () => {
+    it('should expose graphql playground', done => {
+      request(app)
+        .get('/graphql')
+        .set('Accept', 'text/html')
+        .expect(200, done);
+    });
+
+    it('should return ping after a graphql ping query', done => {
+      request(app)
+        .post('/graphql')
+        .send({ query: '{\n  ping\n}', variables: null })
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200)
+        .then(response => {
+          expect(response).toHaveProperty('body');
+          const { body } = response;
+          expect(body).toHaveProperty('data');
+          const { data } = body;
+          expect(data).toHaveProperty('ping', 'ping');
+          done();
+        });
+    });
   });
 
-  it('should return ping after a graphql ping query', done => {
-    request(app)
-      .post('/graphql')
-      .send({ query: '{\n  ping\n}', variables: null })
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200)
-      .then(response => {
-        expect(response).toHaveProperty('body');
-        const { body } = response;
-        expect(body).toHaveProperty('data');
-        const { data } = body;
-        expect(data).toHaveProperty('ping', 'ping');
-        done();
+  describe('GraphQL Controller', () => {
+    describe('Ping service', () => {
+      it('should just return ping', () => {
+        expect(controller).toHaveProperty('ping');
+        expect(controller.ping()).toBe('string');
       });
+    });
   });
 });
