2dd71093ceacf127c362debecec76f82da319439
Alejandro Alcázar <aalcazar@brooktec.com>
2020-03-04 12:29:35 +0100

    [TDD] Test "ping" graphql query

--- server/graphql-api/tests/graphql.test.js
+++ server/graphql-api/tests/graphql.test.js
@@ -8,4 +8,21 @@ describe('GraphQL Server', () => {
       .set('Accept', 'text/html')
       .expect(200, done);
   });
+
+  it('should return ping after a graphql ping query', done => {
+    request(app)
+      .post('/graphql')
+      .send({ query: '{\n  ping\n}', variables: null })
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect(200)
+      .then(response => {
+        expect(response).toHaveProperty('data');
+        const { data } = response;
+        expect(typeof data).toBe('object');
+        expect(data).toHaveProperty('ping', 'ping');
+        done();
+      });
+  });
 });
