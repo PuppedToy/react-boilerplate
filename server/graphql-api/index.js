36b9fcb66434d3899fcffc33efa0c5b55130878e
Alejandro Alcázar <aalcazar@brooktec.com>
2020-03-04 12:54:52 +0100

    Refactor code to remove unnecessary template string

--- server/graphql-api/index.js
+++ server/graphql-api/index.js
@@ -2,9 +2,7 @@ const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const { readFileSync } = require('fs');
 
-const grpahqlSchemaFile = `${String(
-  readFileSync(`${__dirname}/schema.graphql`),
-)}`;
+const grpahqlSchemaFile = String(readFileSync(`${__dirname}/schema.graphql`));
 const schema = buildSchema(grpahqlSchemaFile);
 
 const root = {
