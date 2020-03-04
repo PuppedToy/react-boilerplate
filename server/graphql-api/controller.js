3112d130b88aafdb6a939f75b16c67a7b0c98f7f
Alejandro Alcázar <aalcazar@brooktec.com>
2020-03-04 14:11:11 +0100

    Pass tests "Get to do list should return a list with the same item that was returned with a addTodo" and "Reset should make next getTodoList call to return an empty list"

--- server/graphql-api/controller.js
+++ server/graphql-api/controller.js
@@ -3,19 +3,22 @@ function ping() {
 }
 
 let idCounter = 1;
+let todoList = [];
 
 function addTodo({ title }) {
   const newItem = { title, id: idCounter, done: false };
+  todoList.push(newItem);
   idCounter += 1;
   return newItem;
 }
 
 function getTodoList() {
-  return [];
+  return todoList;
 }
 
 function reset() {
   idCounter = 1;
+  todoList = [];
 }
 
 const controller = {
