1018e1f3a46affc2fa520369237fbd16c0019805
Alejandro Alcázar <aalcazar@brooktec.com>
2020-03-04 12:58:47 +0100

    Use controller and pass ping test

--- server/graphql-api/controller.js
+++ server/graphql-api/controller.js
@@ -1,3 +1,5 @@
-const controller = {};
+const controller = {
+  ping: () => 'ping',
+};
 
 module.exports = controller;
--- server/graphql-api/index.js
+++ server/graphql-api/index.js
@@ -2,16 +2,14 @@ const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const { readFileSync } = require('fs');
 
+const controller = require('./controller');
+
 const grpahqlSchemaFile = String(readFileSync(`${__dirname}/schema.graphql`));
 const schema = buildSchema(grpahqlSchemaFile);
 
-const root = {
-  ping: () => 'ping',
-};
-
 const graphqlMiddleware = graphqlHTTP({
   schema,
-  rootValue: root,
+  rootValue: controller,
   graphiql: true,
 });
 
